b0ffab49152087096bc74824f799a1d77c336012
ichiin <philippeduvalts2@gmail.com>
2023-01-29 23:19:48 +0100

    chore: reordered css

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,21 +1,21 @@
-import { colors, typography } from "utils/colors";
+import { colors, typography } from 'utils/colors';
 
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
 const Container = styled.button`
   align-items: center;
   background-color: ${colors.dustyRed};
   border: none;
   border-radius: 12px;
-  cursor: pointer;
-  outline: inherit;
-  color: ${colors.lilyWhite};
   display: flex;
-  font-family: "Lato", sans-serif;
+  color: ${colors.lilyWhite};
+  cursor: pointer;
+  font-family: 'Lato', sans-serif;
   font-size: ${typography.button.fontSize};
   font-weight: ${typography.button.fontWeight};
   height: 100%;
   justify-content: center;
+  outline: inherit;
   padding: 12px 24px;
   width: 100%;
 `;
